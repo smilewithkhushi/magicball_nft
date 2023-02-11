4f7dcc38a54223187a7a7056d16002403b36f412
Rachit Khurana <70265590+notnotrachit@users.noreply.github.com>
2023-02-11 17:55:46 +0530

    added flask

---
+++ server.py
@@ -0,0 +1,12 @@
+import flask
+from flask import request, jsonify
+from flask import render_template as render
+
+app = flask.Flask(__name__,
+    template_folder='templates',
+    static_folder='static')
+app.config["DEBUG"] = True
+
+@app.route('/', methods=['GET'])
+def home():
+    return render('index.html')
