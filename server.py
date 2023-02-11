c39a1c98e61ac6db2d594f8a85af674f1831ede1
Rachit Khurana <70265590+notnotrachit@users.noreply.github.com>
2023-02-11 21:15:27 +0530

    response page

--- server.py
+++ server.py
@@ -1,7 +1,6 @@
 import flask
 from flask import request, jsonify
 from flask import render_template as render
-import requests
 import random
 
 app = flask.Flask(__name__,
@@ -11,20 +10,24 @@ app.config["DEBUG"] = True
 
 @app.route('/', methods=['GET'])
 def home():
-    return render('index.html')
+    return render('index2.html')
 
-
-def q_return(q,r):
-    return render("response.html",q=q,r=r)
 
 @app.route('/question/submit', methods=['POST'])
 def q_submit():
-    question=requests.form['question']
-    responses=['Yes','No']
+    question=request.form['question']
+    responses= ['It is certain', "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes", "Most likely",
+    "Outlook good", "Yes", "Signs point to yes", "Reply hazy, try again", "Ask again later", "Better not tell you now", "Cannot predict now",
+    "Concentate and ask again", "Don't count on it", "My reply is no", "Outlook not so good", "Very doubtful"]
     respond=random.choice(responses)
-    q_return(question,respond)
+    return render("response.html",question=question,response=respond)
 
 
 
+@app.route("/mint", methods=['POST'])
+def mint():
+    question=request.form['question']
+    response=request.form['response']
+    return render("minted.html",id="")
 
 app.run()
